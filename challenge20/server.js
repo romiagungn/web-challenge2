6ce768d1af322b69da90f994d7ceec5deadcd70c
romiagungn <romian1394@gmail.com>
2020-03-02 00:50:51 +0700

    modified server edit

--- challenge20/server.js
+++ challenge20/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const sqlite3 = require('sqlite3').verbose();
 const fs = require("fs");
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 const db_name = path.join(__dirname, "bread.db");
 const db = new sqlite3.Database(db_name, err => {
@@ -41,7 +41,7 @@ app.post('/add',(req, res) => {
   };
   let sql = "INSERT INTO crud (id, string, integer, float, date, boolean) VALUES (?,?,?,?,?,?)";
   let param = [data.id, data.string, data.integer, data.float, data.date, data.boolean];
-  db.run(sql, param, (err) => {
+  db.run(sql, param, (err,) => {
     if (err) {
     return console.error(err.message);
         }
@@ -49,10 +49,32 @@ app.post('/add',(req, res) => {
     });
 });
 
-app.get('/delete/:id',(req, res) => {
-  const id = req.params.id;
-  let sql = `DELETE FROM crud WHERE id = ${id}`;
-  db.run(sql, (err) => {
+app.post('/edit', (req,res) => {
+  // const data = {
+  //   id: req.body.id,
+  //   string: req.body.string,
+  //   integer: req.body.integer,
+  //   float: req.body.float,
+  //   date: req.body.date,
+  //   boolean: req.body.boolean
+  // };
+
+  // let sqleditData = `UPDATE crud SET string = ?, integer = ?, float = ?, date = ?, boolean = ? WHERE id = ?`;
+  let sqleditData2 = "UPDATE crud SET  string '"+req.body.string+"' WHERE id = "+req.body.id;
+  // let param = [data.string, data.integer, data.float, data.date, data.boolean, data.id];
+      db.run(sqleditData2, (err) => {
+        if (err) {
+        return console.error(err.message);
+            }
+            res.redirect('/');
+        });
+
+})
+
+app.post('/hapus',(req, res) => {
+  const id = req.body.id;
+  let sqlDelete = `DELETE FROM crud WHERE id = ?`;
+  db.run(sqlDelete, id, (err) => {
     if (err) {
     return console.error(err.message);
         }
@@ -62,7 +84,7 @@ app.get('/delete/:id',(req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log("Port ini berjalan di localhost:3000");
+app.listen(8000, () => {
+    console.log("Port ini berjalan di localhost:8000");
 });
 
