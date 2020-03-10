c1e1e7fdcd17724646914cb024703294fa90f98f
romiagungn <romian1394@gmail.com>
2020-03-10 20:53:06 +0700

    start challenge 22 mongodb with crud

--- challenge22/app.js
+++ challenge22/app.js
@@ -1,18 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// create app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// use middleware
+app.use(cors())
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
+// use indexroutes
+app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,12 +39,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +53,16 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// config connection to mongodb
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/breaddb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true
+})
+.then(() => console.log('DB connected successfully'))
+.catch((err) => console.error(err))
 
 module.exports = app;
--- challenge22/routes/index.js
+++ challenge22/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'ASEP BAUD SEDUNIA AJA',
+    gelar: 'RAJA DARI SEMUA BETON',
+    nama: 'ASEP BETON',
+    saudara: 'SAUDARA JAUH DARI ASEP BAUD'
+  });
 });
 
 module.exports = router;
